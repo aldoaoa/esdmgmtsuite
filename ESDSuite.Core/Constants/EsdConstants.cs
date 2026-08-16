1c3e817a81822a29f7b821ff21b4a888f6347371
Aldo Orozco <aldoaoa@192.168.1.14>
2026-08-16 09:32:52 -0600

    chore: bump version to v1.6.1 in EsdConstants and Index.cshtml badges

--- ESDSuite.Core/Constants/EsdConstants.cs
+++ ESDSuite.Core/Constants/EsdConstants.cs
@@ -4,7 +4,7 @@ namespace ESDSuite.Core.Constants;
 
 public static class EsdConstants
 {
-    public const string SystemVersion = "1.6.0";
+    public const string SystemVersion = "1.6.1";
 
     public static readonly Dictionary<string, EsdElementInfo> InfoElementosEsd = new()
     {
