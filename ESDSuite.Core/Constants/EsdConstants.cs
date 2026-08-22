e9a504a67dc76a168616330b638f51b2b0b9e650
Aldo Orozco <aldoaoa@192.168.1.32>
2026-08-21 23:08:54 -0600

    fix(inventory): dynamically populate lines by active site in asset edit modal and normalize category selection v2.4.1

--- ESDSuite.Core/Constants/EsdConstants.cs
+++ ESDSuite.Core/Constants/EsdConstants.cs
@@ -4,7 +4,7 @@ namespace ESDSuite.Core.Constants;
 
 public static class EsdConstants
 {
-    public const string SystemVersion = "2.4.0";
+    public const string SystemVersion = "2.4.1";
 
     public static readonly Dictionary<string, EsdElementInfo> InfoElementosEsd = new()
     {
